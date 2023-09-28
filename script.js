9cdd2bae7e47560b2aaf036022857d9a3d52cdde
Cristina Mateo <cris.mateo.99@gmail.com>
2023-09-28 12:11:12 +0200

    algo no funciona?

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Diseña un algoritmo que cuente las veces que aparece una determinada letra en una frase.
+//1) Diseña un algoritmo que cuente las veces que aparece una determinada letra en una frase.
 
 function contarLetras(frase, letra){
     let total = [];
@@ -16,3 +16,25 @@ console.log(contarLetras("en este no hay nada que buscar", "f"))// espected outp
 console.log(contarLetras("no uppercase in here", "E"))//expected output: 4
 
 //Creo la funcion contarLetras que acepta una frase y una letra. En ella hay un array vacío que va a servir para almacenar la cantidad de veces que aparece determinada letra. Incluyo un condicional (if) en el que convierte la "frase" en un string,  pongo este paso por si a alguien, por algún motivo, se le ocurre insertar una frase en forma de array. Paso un bucle por la frase que itere por todo su largo i si un elemento de la frase (puesta en minúscula) en la iteración coincide con la letra(también en minúscula), se añade i al array vacío del principio. También podría añadir frase[i], pero así almacena en el "total" la posición en la que se encuentra esa letra y es más sencillo de seguir. La funcion debe devolverme el largo del "total", que es la cantidad de veces que aparece la letra.
+
+
+//2)Diseña un algoritmo que imprima los números impares entre un número dado por el usuario y los siguientes 50 números
+
+
+function impares(){
+    //let num = parseInt(prompt("Introduzca un número entero."));
+    let num = Math.floor(Math.random() * 1)
+    let masCincuenta = (num+50)
+     let arr =[]
+     while(num <= masCincuenta){
+       arr.push(num++)
+ } 
+ let resultado =[]
+     for(i=0;i<arr.length;i++){
+         if(arr[i] % 2 !== 0){
+             resultado.push(arr[i])
+         }
+     }
+     return resultado
+ }
+ 
